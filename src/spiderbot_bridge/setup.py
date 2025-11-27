c99427ffa12149e6ef4d70451f2763a8a97bf1b7
SpiderbotPi <spiderbot@pi.local>
2025-11-26 23:23:13 -0700

    first update

---
+++ src/spiderbot_bridge/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'spiderbot_bridge'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='vijay',
+    maintainer_email='vijay@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+		'pi_bridge_node = spiderbot_bridge.pi_bridge_node:main',
+        ],
+    },
+)
